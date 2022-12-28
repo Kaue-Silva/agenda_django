a691ae5f4fe0ba49d9d2ebc80b3d79202ea24d99
Kaue-Silva <kauesilva940@gmail.com>
2022-12-28 19:10:31 -0300

    feature: update items agenda

--- agenda_app/urls.py
+++ agenda_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AgendaList, AgendaCreate
+from .views import AgendaList, AgendaCreate,  AgendaUpdate
 
 urlpatterns = [
     path("list/", AgendaList.as_view(), name="agenda_list"),
     path("create/", AgendaCreate.as_view(), name="agenda_create"),
+    path("update/<int:pk>", AgendaUpdate.as_view(), name="agenda_update")
 ]
--- agenda_app/views.py
+++ agenda_app/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Agenda
 from .forms import AgendaForm
 from django.urls import reverse_lazy
@@ -11,6 +11,14 @@ class AgendaList(ListView):
 
 class AgendaCreate(CreateView):
     template_name = "agenda_app/create_agenda.html"
+    form_class = AgendaForm
+    
+    def get_success_url(self):
+        return reverse_lazy("agenda_list")
+
+class AgendaUpdate(UpdateView):
+    template_name = "agenda_app/Update_agenda.html"
+    model = Agenda
     form_class = AgendaForm
     
     def get_success_url(self):
