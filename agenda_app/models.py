35749f776e57852e63b65b7fde5dd129d7a9039b
Kaue-Silva <kauesilva940@gmail.com>
2022-12-28 19:21:57 -0300

    add: new camp for number

--- agenda_app/models.py
+++ agenda_app/models.py
@@ -5,6 +5,10 @@ class Agenda(models.Model):
     compromisso = models.CharField(max_length=255)
     data_hora = models.DateTimeField()
     cliente = models.CharField(max_length=255)
+    celular = models.CharField(max_length=11)
     
     class Meta:
         db_table = "agenda"
+        
+    def __str__(self):
+        return f"{self.compromisso} {self.cliente}"
